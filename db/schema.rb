f9a0ab51ad44d02a1b08248153fd9952a390273a
Sowjanya <nagas85@gmail.com>
2013-01-24 18:25:44 -0800

    update to votes controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124230856) do
+ActiveRecord::Schema.define(:version => 20130125010327) do
 
   create_table "topics", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
